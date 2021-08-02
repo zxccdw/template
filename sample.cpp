479c1bfb4b9c48f0e68828560f3abe772f008205
zxccdw <57716489+zxccdw@users.noreply.github.com>
2021-08-02 11:55:37 +0400

    Create sample.cpp

---
+++ sample.cpp
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <math.h>
+#include <map>
+#include <chrono>
+#include <set>
+#include <stack>
+#include <vector>
+#include <time.h>
+#include <random>
+#include <algorithm>
+#include <string>
+#include <string.h>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cctype>
+#include <list>
+#include <unordered_set>
+#include <unordered_map>
+#include <bitset>
+#include <complex>
+using namespace std;
+typedef long long ll;
+mt19937 rng(time(0)); uniform_int_distribution<int>uid(-1e9, 1e9);
+vector<int> g[5005], res; int u[5005];
+
+
+signed main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+	
+}
